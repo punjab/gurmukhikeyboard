482393dbed13db60964772947761e76f832aa61a
Thomas Yoon <HiroiSekai@Stardrop.local>
2015-01-08 09:14:58 -0800

    blog page designed

---
+++ design/script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+
+});
+
+});
